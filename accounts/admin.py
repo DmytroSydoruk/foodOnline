fc2133451789e64d1e92197673524105e5ead93c
Dmytro Sydoruk <dima.sudoryk@gmail.com>
2023-01-09 15:47:40 +0200

    vendor registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email','first_name', 'last_name', 'role')
+    list_display = ('email','first_name', 'last_name','username', 'role', 'is_active')
     ordering = ('-date_joined',)
     filter_horizontal = ()
     list_filter = ()
@@ -12,5 +12,5 @@ class CustomUserAdmin(UserAdmin):
 
 
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(UserProfile)
