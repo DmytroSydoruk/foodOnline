66f2051cbbf4629ffb8bae58cf3625200e2e7ec2
Dmytro Sydoruk <dima.sudoryk@gmail.com>
2023-01-12 02:56:28 +0200

    setup vendor profile page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
@@ -14,4 +17,30 @@ class UserForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
-            raise forms.ValidationError('Password does not match confirm password')
+            raise forms.ValidationError(
+                'Password does not match confirm password')
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    # latitute = forms.CharField(widget=forms.TextInput(
+    #     attrs={'readonly': 'readonly'}))
+    # longitute = forms.CharField(widget=forms.TextInput(
+    #     attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'post_code',
+                  'latitute', 'longitute']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitute' or field == 'longitute':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
